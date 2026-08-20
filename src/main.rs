42ad97e1199e141b9ae3dc7e35456e22e234f2bb
wangyinhe <wyinhe@sina.cn>
2026-08-20 18:54:01 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process::Command;
 
 fn main() {
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    let mut input = String::with_capacity(32);
+    io::stdin().read_line(&mut input).unwrap();
+    let cmd = input.trim();
+
+    Command::new(cmd).spawn().unwrap();
 }
