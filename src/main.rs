ce42441b7898715decec8d266df61627bff049cb
wangyinhe <wyinhe@sina.cn>
2026-08-23 16:08:19 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,10 @@ use std::path::PathBuf;
 use std::process::Command;
 use which::which;
 
+use crate::redirect::Redirection;
+
+pub mod redirect;
+
 pub enum ProgramType {
     Builtin,
     External(PathBuf),
@@ -13,77 +17,151 @@ pub enum ProgramType {
 }
 
 pub enum RunState {
-    Exit,
-    Continue,
+    Out(String),
+    Error(String),
+    None,
+}
+
+pub struct CommandResult {
+    exit: bool,
+    state: RunState,
+}
+
+impl CommandResult {
+    fn new(exit: bool, state: RunState) -> Self {
+        Self { exit, state }
+    }
+
+    fn exit(state: RunState) -> Self {
+        Self::new(true, state)
+    }
+
+    fn proceed(state: RunState) -> Self {
+        Self::new(false, state)
+    }
 }
 
 fn main() {
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
-
         let mut input = String::with_capacity(32);
         io::stdin().read_line(&mut input).unwrap();
 
-        match run(input) {
-            Err(e) => eprintln!("{:#}", e),
-            Ok(state) => match state {
-                RunState::Continue => continue,
-                RunState::Exit => return,
-            },
+        let parts = match shell_words::split(&input).context("command line parse error.") {
+            Ok(parts) => parts,
+            Err(e) => {
+                eprintln!("{:#}", e);
+                continue;
+            }
+        };
+        if parts.is_empty() {
+            continue;
+        }
+
+        let cmd = parts[0].as_str();
+        let (args, redirects) = match redirect::parse_redirections(&parts[1..]) {
+            Ok((args, redirects)) => (args, redirects),
+            Err(e) => {
+                eprintln!("{:#}", e);
+                continue;
+            }
+        };
+
+        match run_command(cmd, &args) {
+            Err(e) => eprint!("{:#}", e),
+            Ok(cmd_res) => {
+                if let Err(e) = handle_output(cmd_res.state, &redirects) {
+                    eprintln!("{:#}", e);
+                }
+
+                if cmd_res.exit {
+                    break;
+                }
+            }
         }
     }
 }
 
-fn run(input: String) -> Result<RunState> {
-    let parts = shell_words::split(&input).context("command line parse error.")?;
-    if parts.is_empty() {
-        return Ok(RunState::Continue);
-    }
-    let cmd = parts[0].as_str();
-    let args = &parts[1..];
+fn handle_output(state: RunState, redirects: &[Redirection]) -> Result<()> {
+    let mut out_handled = false;
+    let mut err_handled = false;
 
-    match cmd {
-        "echo" => println!("{}", args.join(" ")),
-        "type" => {
-            if !args.is_empty() {
-                let arg = &args[0];
-                match my_which(arg) {
-                    ProgramType::Builtin => println!("{arg} is a shell builtin"),
-                    ProgramType::External(path) => println!("{} is {}", arg, path.display()),
-                    _ => println!("{arg}: not found"),
-                }
+    for redirect in redirects {
+        redirect.handle(&state, &mut out_handled, &mut err_handled)?;
+    }
+
+    match state {
+        RunState::Out(msg) => {
+            if !out_handled {
+                print!("{msg}");
             }
         }
-        "pwd" => println!("{}", env::current_dir()?.display()),
+        RunState::Error(msg) => {
+            if !err_handled {
+                eprint!("{msg}");
+            }
+        }
+        _ => {}
+    };
+    Ok(())
+}
+
+fn run_command(cmd: &str, args: &[&str]) -> Result<CommandResult> {
+    let cmd_res = match cmd {
+        "echo" => CommandResult::proceed(RunState::Out(format!("{}\n", args.join(" ")))),
+        "type" => {
+            if let Some(arg) = args.first() {
+                let out = match my_which(arg) {
+                    ProgramType::Builtin => format!("{arg} is a shell builtin\n"),
+                    ProgramType::External(path) => format!("{} is {}\n", arg, path.display()),
+                    _ => format!("{arg}: not found\n"),
+                };
+                CommandResult::proceed(RunState::Out(out))
+            } else {
+                CommandResult::proceed(RunState::None)
+            }
+        }
+        "pwd" => CommandResult::proceed(RunState::Out(format!(
+            "{}\n",
+            env::current_dir()?.display()
+        ))),
         "cd" => {
-            if !args.is_empty() {
-                let arg = &args[0];
+            let mut state = RunState::None;
+            if let Some(&arg) = args.first() {
                 let result = if arg == "~" {
                     let home = env::var("HOME")?;
                     env::set_current_dir(home)
                 } else {
                     env::set_current_dir(arg)
                 };
-                result.unwrap_or_else(|_| println!("cd: {}: No such file or directory", arg));
+
+                if let Err(_) = result {
+                    state = RunState::Out(format!("cd: {}: No such file or directory\n", arg));
+                }
             }
+            CommandResult::proceed(state)
         }
-        "exit" => return Ok(RunState::Exit),
+        "exit" => CommandResult::exit(RunState::None),
         _ => match which(cmd) {
             Ok(_) => {
                 let output = Command::new(cmd).args(args).output()?;
                 if output.status.success() {
-                    print!("{}", String::from_utf8(output.stdout)?);
+                    CommandResult::proceed(RunState::Out(String::from_utf8(output.stdout)?))
                 } else {
                     let stderr = String::from_utf8_lossy(&output.stderr);
-                    eprintln!("execute fail! exit code: {:?}", output.status.code());
-                    eprintln!("error info:\n{}", stderr);
+                    let msg = format!(
+                        "execute fail! exit code: {:?}\nerror info:\n{}",
+                        output.status.code(),
+                        stderr
+                    );
+                    CommandResult::proceed(RunState::Error(msg))
                 }
             }
-            Err(_) => println!("{cmd}: command not found"),
+            Err(_) => CommandResult::proceed(RunState::Out(format!("{cmd}: command not found\n"))),
         },
-    }
-    Ok(RunState::Continue)
+    };
+    Ok(cmd_res)
 }
 
 fn my_which(cmd: &str) -> ProgramType {
---
+++ src/redirect.rs
@@ -0,0 +1,72 @@
+use anyhow::{Result, bail};
+use std::fs::File;
+use std::io::Write;
+use std::path::PathBuf;
+
+use crate::RunState;
+
+pub enum RedirectType {
+    StdoutTruncate, // > 或 1> (覆盖标准输出)
+    StderrTruncate, // 2> (覆盖标准错误)
+}
+
+pub struct Redirection {
+    pub rtype: RedirectType,
+    pub file_path: PathBuf,
+}
+
+impl Redirection {
+    pub fn handle(
+        &self,
+        state: &RunState,
+        out_handled: &mut bool,
+        err_handled: &mut bool,
+    ) -> Result<()> {
+        match self.rtype {
+            RedirectType::StdoutTruncate => {
+                if let RunState::Out(msg) = state {
+                    let mut f = File::create(&self.file_path)?;
+                    f.write_all(msg.as_bytes())?;
+                    *out_handled = true;
+                }
+            }
+            RedirectType::StderrTruncate => {
+                if let RunState::Error(msg) = state {
+                    let mut f = File::create(&self.file_path)?;
+                    f.write_all(msg.as_bytes())?;
+                    *err_handled = true;
+                }
+            }
+        }
+        Ok(())
+    }
+}
+
+pub fn parse_redirections(tokens: &[String]) -> Result<(Vec<&str>, Vec<Redirection>)> {
+    let mut redirects = Vec::new();
+    let mut args: Vec<&str> = Vec::with_capacity(tokens.len());
+    let mut iter = tokens.iter();
+    while let Some(token) = iter.next() {
+        let rtype = match token.as_str() {
+            ">" | "1>" => Some(RedirectType::StdoutTruncate),
+            "2>" => Some(RedirectType::StderrTruncate),
+            _ => None,
+        };
+
+        if let Some(t) = rtype {
+            if let Some(file) = iter.next() {
+                let redirection = Redirection {
+                    rtype: t,
+                    file_path: PathBuf::from(file),
+                };
+                redirects.push(redirection);
+            } else {
+                bail!("parse error: miss redirect file name!");
+            }
+        } else {
+            args.push(token);
+        }
+    }
+
+    Ok((args, redirects))
+}
